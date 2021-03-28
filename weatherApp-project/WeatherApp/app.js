c827e3fd8399f2c5c38883e1b03501df50c1fe28
Raphael Pereira <75498332+RaphaelPereira88@users.noreply.github.com>
2021-03-28 13:27:53 +0100

    Update app.js

--- weatherApp-project/WeatherApp/app.js
+++ weatherApp-project/WeatherApp/app.js
@@ -1,5 +1,7 @@
 
-
+document.getElementById("country").addEventListener('click',()=>{
+    document.getElementById("country").style.borderColor = "black";
+});
 
 /* Event listener that will trigger the API call function and gather all the user datas and API 
 datas that the server will then add to the js object projectData */
@@ -7,7 +9,11 @@ document.getElementById('generate').addEventListener('click',action);
 
 function action(e){
     const newZip = document.getElementById('zip').value;
+    const country= document.getElementById('country').value;
     const newContent = document.getElementById('feelings').value;
+    let countryComma = ',';
+    let baseURL ='https://api.openweathermap.org/data/2.5/weather?q=';
+    let apiKey = "&appid=c3ce4cd3a2dad80331ebdf71257604b6";
 
     /*Create a new date instance dynamically with JS*/
     let d = new Date();
@@ -15,25 +21,30 @@ function action(e){
     document.getElementById("date").value = newDate;
     const newTime = document.getElementById("date").value;
     
-    getCity(baseURL, newZip, apiKey)
-    /*to tell the program to do this task after the previous one */
-    .then(function(data){
-        postData("/save", { date:newTime, temp:data.main.temp, content:newContent })
-    /*to get the data from projectdata (the js object) displayed on the browser*/
-    .then(updateUI())
-    })
+    if (country ==="0" ){
+        alert("please select a country first");
+        document.getElementById("country").style.borderColor = "red";
+        window.stop();
+    }else{
+        getCity(baseURL, newZip,countryComma,country, apiKey)
+        /*to tell the program to do this task after the previous one */
+        .then(function(data){
+            postData("/save", { date:newTime, name: data.name, temp:data.main.temp, description:data.weather[0].description, content:newContent })
+        /*to get the data from projectdata (the js object) displayed on the browser*/
+        .then(updateUI())
+        });
+    }
 }
 
 
 /*API Call function*/
 /*Get request function using Fetch to make a dynamic url query*/
-let baseURL ='https://api.openweathermap.org/data/2.5/weather?q=';
-let apiKey = "&appid=c3ce4cd3a2dad80331ebdf71257604b6";
 
-const getCity = async (baseURL, zip, key) =>{
-    const res = await fetch(baseURL+ zip+ key )
+const getCity = async (baseURL, zip, countryComma, country, key) =>{
+    const res = await fetch(baseURL+ zip + countryComma + country + key )
     try{
         const data = await res.json();
+        console.log(data);
         return data
     }
     catch(error){
@@ -64,11 +75,12 @@ const updateUI = async() => {
     const request = await fetch("/all")
     try {
         const allData = await request.json();
-        document.getElementById('date').innerHTML = allData[0].date;
-        document.getElementById('temp').innerHTML = allData[0].temp;
-        document.getElementById('content').innerHTML = allData[0].content;
+        document.getElementById('date').innerHTML = allData.date;
+        document.getElementById('temp').innerHTML = allData.temp;
+        document.getElementById('name').innerHTML = allData.name;
+        document.getElementById('description').innerHTML = allData.description;
+        document.getElementById('content').innerHTML = allData.content;
     }catch(error){
         console.log("error", error);
     }
 }
-
